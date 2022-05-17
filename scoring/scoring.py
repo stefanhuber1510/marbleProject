32bb2aa64de4a899413005d23dcb07a923363b2e
stefanhuber1510 <stefan.huber3125@gmail.com>
2022-05-17 12:47:42 +0200

    idk

---
+++ scoring/scoring.py
@@ -0,0 +1,18 @@
+# input: obs is a Dataframe with 3 Columns: 1. A combination idx for the 2 drugs, y_test, pred
+def primaryMetric(obs):
+    import numpy as np
+    weighted_corr = []
+    weights = []
+    for i in set(obs['combination_idx']):
+
+        n = len(obs.loc[obs['combination_idx'] == i])
+
+        if (np.var(obs["y_test"][obs['combination_idx'] == i]) == 0 and n > 1):
+            weighted_corr.append(0)
+            weights.append(np.sqrt(n-1))
+        elif (n > 1):
+            weighted_corr.append(np.sqrt(n-1) * np.corrcoef(
+                obs.loc[obs['combination_idx'] == i]["y_test"], obs.loc[obs['combination_idx'] == i]["pred"])[0, 1])
+            weights.append(np.sqrt(n-1))
+    df = pd.DataFrame({"corr": weighted_corr, "weights": weights}).dropna()
+    return(sum(df["corr"])/sum(df["weights"]))
